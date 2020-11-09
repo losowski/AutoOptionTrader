3a3a9a9be344974c5bcf3149adf0ad3bdc21d73e
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-11-09 20:46:52 +0000

    Added reinforrcementLearning class

---
+++ python/RLModel/reinforcementLearning.py
@@ -0,0 +1,17 @@
+# Main class for handling the reinforcement learning
+# Hosts all the stages
+
+import logging
+
+class ReinforcementLearning (object):
+	def __init__(self):
+		pass
+
+	def __del__(self):
+		pass
+
+	def trainSteps(self):
+		#	1) basic rules (observation changes to reward) (passive)
+		#	2) Actions -> Observation changes (playing)
+		#	3) Learning how the Actions can interact with the observation changes to get the rewards
+		pass

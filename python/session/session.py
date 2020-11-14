3c6aca0b440da710e2054514562738674a370a65
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-11-14 17:39:40 +0000

    Added session

--- python/session/session.py
+++ python/session/session.py
@@ -5,13 +5,18 @@ import threading
 
 from python.comms import client
 
+from python.proto import game_pb2
+
 class Session (client.Client):
 	def __init__(self, tradeAddr, tradePort):
 		super(Session, self).__init__(tradeAddr, tradePort)
 		self.logger         =   logging.getLogger('Session')
 		self.thread			=	None
 		self.live			=	True
-
+		# Game variables
+		self.gameMeta		=	game_pb2.gameMeta()
+		self.gameResponse	=	None
+		self.gameRequest	=	game_pb2.gameRequest()
 
 	def __del__(self):
 		super(Session, self).__del__()
@@ -39,8 +44,27 @@ class Session (client.Client):
 		self.thread.join()
 
 
+	# Reset Game data except for metadata
+	def resetRequest(self):
+		if (self.gameResponse is not None):
+			if (self.gameResponse.HasField('meta')):
+				# Store the response meta data
+				self.gameMeta.CopyFrom(self.gameResponse.meta)
+		# Reset the main request
+		self.gameRequest	=	game_pb2.gameRequest()
+		# Restore the metaData
+		self.gameRequest.meta.CopyFrom(self.gameMeta)
+
+
 	# Session Running
 	def run(self):
 		self.logger.info("Thread running")
+		# 1) Send the metadata to choose the game to play
+		# 2) Get response and store the metadata we are playing on
 		while (self.live):
 			self.logger.debug("Running the trading game")
+			# Check the response
+			if (self.gameResponse.status != game_pb2.gameStatus.OK):
+				self.logger.info("Response Bad")
+				# Kill the session
+				self.live = False

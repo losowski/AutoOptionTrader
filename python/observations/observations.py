81e07d94a38d998fd25179da8aac0af7ef692419
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-11-18 20:32:28 +0000

    Cleaned up the observation part of the code

--- python/observations/observations.py
+++ python/observations/observations.py
@@ -1,21 +1,65 @@
 # Observation base class
+#	Observations are lists of values
 import logging
 
-class Observations (object):
+class Observation (object):
 	def __init__(self):
-		self.logger         =   logging.getLogger('Observations')
-		self.observations		=	None
-		self.deltaObservations	=	None
+		self.logger					=   logging.getLogger('Observation')
+		self.observation			=	None
+		self.previousObservation	=	None
+		self.deltaObservation		=	None
+		self.changed				=	False
 
 
 	def __del__(self):
 		pass
 
 
+	# Set the current observation
+	#	Only update the previous Observation pattern if this has changed
+	def updateObservation(self, observation):
+		# Store the current observation locally
+		obs	=	observation
+		# Check if has changed
+		self.__diffObservations(obs)
+		if (self.hasChanged() == True):
+			self.previousObservation = self.observation
+			self.observation = observation
+
+
+	# Confirm if the state has changed
+	def hasChanged(self):
+		return self.changed
+
+
+	# Diff the observations
+	def __diffObservations(self, newObservation):
+		self.changed = False
+		if(len(self.observation) == len(newObservation)):
+			self.logger.debug("Comparison in length OK")
+			diff = list()
+			for i,x in enumerate(self.observation):
+				# Element comparator
+				try:
+					# Try a numerical comparator
+					d  = int(newObservation[i]) - int(x)
+				except:
+					# For WTF moments of bad encoding
+					self.logger.warning("Bad comparator: %s - %s", newObservation[i], x)
+					d = -1	# -1 represents default change
+				# Check for changes
+				if (diff == 0):
+					self.changed = True
+				# Build the self.changed
+				diff.append(d)
+			if (self.changed == True):
+				self.deltaObservation = diff
+		else:
+			self.logger.error("Observations of different lengths")
+			self.changed = True
+
+
 	# Get the change in observation
-	def getDeltaObservations(self, observations):
-		# - used for iterative differences in values across the board
-		#	NOTE: might need proper implementation
-		self.deltaObservations = observations - self.observations
+	def getDeltaObservation(self, observation):
 		#Return the calculated differences
-		return self.deltaObservations
+		return self.deltaObservation

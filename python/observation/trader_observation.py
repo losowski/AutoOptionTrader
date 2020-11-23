e13414c54eb767873a6771a7939b9da82b96ec42
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2020-11-23 22:02:01 +0000

    Added Observation processing of the game data

--- python/observation/trader_observation.py
+++ python/observation/trader_observation.py
@@ -2,18 +2,24 @@
 #	Observations are lists of values
 import logging
 
+# Observation base class
 from python.observation import observation
+
+# Message handling
+from python.proto import game_pb2
 
 class TraderObservation (observation.Observation):
 	def __init__(self):
-		self.logger					=   logging.getLogger('TraderObservation')
 		super(TraderObservation, self).__init__()
+		self.logger					=   logging.getLogger('TraderObservation')
+		selg.gameState				=	game_pb2.gameState()
 
 	def __del__(self):
 		super(TraderObservation, self).__del__()
-		pass
+
 
 	# Generic BaseClass function for parsing a response
+	# Passed in game_pb2.gameResponse
 	def parseResponse(self, message):
-		# Implement custom code here
-		pass
+		if (message.HasField('state')):
+			self.gameState.CopyFrom(message.state)
